2e24b0cdf0b0edf94eb460eb6145af4e26287837
am0ren77 <amoren77@calpoly.edu>
2024-10-05 17:16:30 -0700

    Using new files

--- schoolsearch.py
+++ schoolsearch.py
@@ -6,3 +6,51 @@ import sys
 
 students = []
 teachers = {}
+
+def read_students(filename):
+    students = []
+    try:
+        with open(filename, 'r') as file:
+           for line in file:
+	       data = line.strip().split(',')
+	       if len(data) != 6:
+		  sys.exit(1)
+
+	    student_info = [
+		data[0],		# StLastName
+		data[1],		# StFirstName
+		int(data[2]),		# Grade
+		int(data[3]),		# Classroom
+		int(data[4]),		# Bus
+		float(data[5]),		# GPA
+	    ]
+	    students.append(student_info)
+
+    except FileNotFoundError:
+	sys.exit(1)
+    except ValueError
+	sys.exit(1)
+
+    return students
+
+
+def read_teachers(filename):
+    teachers = {}
+    try:
+        with open(filename, 'r') as file:
+            for line in file:
+                data = line.strip().split(',')
+                if len(data) != 3:
+                    print("Invalid data format in teachers list.")
+                    sys.exit(1)
+	
+		classroom = int(data[2])
+                teacher_info = data[0] + " " + data[1]  #TLastName #
+		teachers[classroom] = teacher_info
+
+    except FileNotFoundError:
+        sys.exit(1)
+
+    return teachers
+
+
