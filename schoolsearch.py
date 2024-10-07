4d2ab7105247bbc228a3b9338dc4c81351de96a7
am0ren77 <amoren77@calpoly.edu>
2024-10-06 17:44:27 -0700

    Working as before

--- schoolsearch.py
+++ schoolsearch.py
@@ -36,7 +36,7 @@ def read_teachers(filename):
                     sys.exit("Incorrect format in teachers file")
 	
 		classroom = int(data[2])
-                teacher_info = data[0] + " " + data[1]  #TLastName #TFirstName
+                teacher_info = data[0] + " " + data[1]  #TLast #TFirst
 		teachers_dict[classroom] = teacher_info
 
     except FileNotFoundError:
@@ -55,10 +55,6 @@ def find_by_last_name(last_name):
             teacher_name = teachers.get(student[3], "Unknown")
             print(student[0] + " " + student[1] + " " + str(student[2]) + 
                   " " + str(student[3]) + " " + teacher_name)
-    else:
-        print("No student found with last name: " + last_name)
-
-
 
 # R5. S[tudent]: <lastname> B[us]
 def find_by_last_name_bus(last_name):
@@ -87,8 +83,29 @@ def find_by_bus(bus):
             print(student[0] + " " + student[1] + " " + str(student[2]) + 
                   " " + str(student[3]))
 
-# R9a.
-# R9b.
+# R9a. G[rade]: <Number> H[igh]
+def find_highest_gpa_in_grade(grade):
+    results = [student for student in students if student[2] == grade]
+    if results:
+        highest_gpa_student = max(results, key=lambda student: student[5])
+        teacher_name = teachers.get(highest_gpa_student[3], "Unknown")
+        print(str(highest_gpa_student[0]) + " " +
+ 	      str(highest_gpa_student[1]) + " " +
+	      str(highest_gpa_student[5]) + " " +
+	      teacher_name 	          + " " +
+	      str(highest_gpa_student[4]))
+
+# R9b. G[rade]: <Number> L[ow]
+def find_lowest_gpa_in_grade(grade):
+    results = [student for student in students if student[2] == grade]
+    if results:
+        lowest_gpa_student = min(results, key=lambda student: student[5])
+        teacher_name = teachers.get(lowest_gpa_student[3], "Unknown")
+        print(str(lowest_gpa_student[0]) + " " +
+              str(lowest_gpa_student[1]) + " " +
+              str(lowest_gpa_student[5]) + " " +
+              teacher_name               + " " +
+              str(lowest_gpa_student[4]))
 
 # R10. A[verage]: <Number>
 def avg_gpa_score(grade):
@@ -129,27 +146,38 @@ def main():
 
         main_command = parts[0]
 
-        if main_command == 'S' and len(parts) > 1:
+        if main_command.startswith('S') and len(parts) > 1:
             last_name = parts[1]
             if len(parts) > 2 and parts[2].startswith('B'):
                 find_by_last_name_bus(last_name)
             else:
                 find_by_last_name(last_name)
 
-	elif main_command == 'T' and len(parts) > 1:
+	elif main_command.startswith('T') and len(parts) > 1:
             tlast_name = parts[1]
             find_by_tlast_name(tlast_name)
+	
+	elif main_command.startswith('G') and len(parts) > 1:
+            grade = int(parts[1])
+            if len(parts) == 2:
+                find_by_grade(grade)
+            elif len(parts) > 2:
+                option = parts[2]
+                if option.startswith('H'):
+                    find_highest_gpa_in_grade(grade)
+                elif option.startswith('L'):
+                    find_lowest_gpa_in_grade(grade)
 
-        elif command.startswith('B'):
+        elif main_command.startswith('B'):
 	  if len(parts) > 1:
 		bus_route = int(parts[1])
 		find_by_bus(bus_route)
 	
-	elif main_command == 'A' and len(parts) > 1:
+	elif main_command.startswith('A') and len(parts) > 1:
             grade = int(parts[1])
             avg_gpa_score(grade)
 
-	elif main_command == 'I':
+	elif main_command.startswith('I'):
             student_info()
 
 
